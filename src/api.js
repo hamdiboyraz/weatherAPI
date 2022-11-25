d3d1e4ab471ee702135bd02791ba3a6a7d181319
hamdiboyraz <hamdiboyraz19@gmail.com>
2022-11-25 20:10:39 +0300

    Fetch and map data from weather API

--- src/api.js
+++
@@ -1,9 +0,0 @@
-export const geoApiOptions = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '136d979fa7msh3d6abaa8d784ce2p10b091jsn0b28b5a4b42b',
-            'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-        }
-    };
-    
-export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
